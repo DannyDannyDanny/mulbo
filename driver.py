20138525e272bfc34185deb120e76928d6ebf714
DannyDannyDanny <s144222@student.dtu.dk>
2020-12-20 16:07:43 +0100

    :construction: work-in-progress on shcheduling timer

--- driver.py
+++ driver.py
@@ -1,29 +1,60 @@
 import config
 from glob import glob
-import soundfile as sf
-import pandas as pd
+import sched
+import time
 import matplotlib.pyplot as plt
 from validate import write_rawfile, get_shazam_tags
 # dark mode plt
 plt.style.use(['dark_background'])
 # print(plt.style.available)
 
-for filepath in glob(config.dirs['originals']+'*'):
-    try:
-        rawfile_data = write_rawfile(filepath)
-        print(f'scannned:\t{filepath}')
-    # except TypeError as e:
-    #     print(f'skipping: {e}')
-    except RuntimeError as e:
-        print(f'skipping:\t{e}')
+
+def get_rawfiles_for_shazzaming():
+    """returns set of rawfiles which have no responses
+    i.e they need to be shazzamed"""
+    raws = glob(config.dirs['rawfiles']+'*')
+    raws = [i.split('/')[-1].split('.')[0] for i in raws]
+    raws = set(raws)
+    resps = glob(config.dirs['responses']+'*')
+    resps = [i.split('/')[-1].split('.')[0] for i in resps]
+    resps = set(resps)
+    return raws - resps
+
+
+# CONVERT TO AUDIO RAWFILES (ALREADY DONE)
+if False:
+    for filepath in glob(config.dirs['originals']+'*'):
+        try:
+            rawfile_data = write_rawfile(filepath)
+            print(f'scannned:\t{filepath}')
+        # except TypeError as e:
+        #     print(f'skipping: {e}')
+        except RuntimeError as e:
+            print(f'skipping:\t{e}')
+
+
+def shazam_some_audio():
+    """retreives some unshazammed rawfile and shazams it"""
+    guid = get_rawfiles_for_shazzaming()
+    if not len(guid):
+        print('nothing to shazam')
+        return None
+    guid = guid.pop()
+    print('will shazam this one->', guid)
+    rawfile = config.dirs['rawfiles'] + guid + '.raw'
+    response = get_shazam_tags(rawfile)
+    print(response.to_markdown)
+    return response
 
 # %%
-for raw in glob(config.dirs['rawfiles']+'*'):
-    kw = dict(channels=1, samplerate=44100, format='RAW', subtype='PCM_16')
-    sig, fs = sf.read(raw, **kw)
-    break
-
-response = get_shazam_tags(raw)
-# %%
-# %%
-pd.read_pickle(resp_path)
+# max 5 per second
+# max 500 per month
+print('retreiving rawfiles for shazzaming')
+raws = get_rawfiles_for_shazzaming()
+print(f'{len(raws)} rawfiles found')
+s = sched.scheduler(time.time, time.sleep)
+for _ in raws:
+    print('adding shazam to queue')
+    s.enter(delay=20, priority=1, action=shazam_some_audio)
+print('starting scheduler!')
+s.run()
