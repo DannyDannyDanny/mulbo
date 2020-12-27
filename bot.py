fe77bd08dd142e5cfc5c4a828ca55bda0cb39b2a
DannyDannyDanny <s144222@student.dtu.dk>
2020-12-27 11:57:56 +0100

    :memo: add logging to bot

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 from config import telegram_token, logging_format
+import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 updater = Updater(token=telegram_token)
 dispatcher = updater.dispatcher
 
+logging.basicConfig(filename='example.log',
+                    format=logging_format,
+                    level=logging.INFO)
+logger = logging.getLogger()
+logging.info('starting machine')
+
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    logging.info('starting machine')
 
 
 def echo(update, context):
